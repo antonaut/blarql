9634605af1f75cfa7e56091f5423c159b0f1ac71
unknown <sfreud@IF501-210-06.insa-lyon.fr>
2014-10-24 11:37:15 +0200

    added prefixes

--- app.js
+++ app.js
@@ -18,6 +18,18 @@ var performSPARQLQuery = function(query, hash) {
 	//	var query = "select distinct ?Concept where {[] a ?Concept} LIMIT 10";
 	var client = new SparqlClient(endpoint);
 
+	var prefixes = "PREFIX owl: <http:\//www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX : <http://dbpedia.org/resource/>
+PREFIX dbpedia2: <http://dbpedia.org/property/>
+PREFIX dbpedia: <http://dbpedia.org/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+";
+	query = prefixes + query;
 	//	console.log("Query to " + endpoint);
 	console.log("Hash: " + hash);
 	console.log("Query: " + query);
